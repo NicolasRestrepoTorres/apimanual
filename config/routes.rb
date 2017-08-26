58e1393a0febeb3fe0527577eed59f837981209f
Nicolas <nrestrepot@unal.edu.co>
2017-08-26 01:19:40 +0000

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :computers
+  resources :diseases
+  resources :beds
+  resources :ducks
+  resources :mice
+  resources :oxen
+  resources :records
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
